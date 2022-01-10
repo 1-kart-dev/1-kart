3d86e159bddfeaeb28fef05fe111579a03acdf0b
willswinyer <will@1-kart.com>
2022-01-10 14:39:00 -0500

    firebase is set up

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,17 @@ import styles from '../styles/Home.module.scss'
 import dynamic from 'next/dynamic'
 import HomePage from './HomePage'
 import Link from 'next/link'
-import * as AWS from 'aws-sdk';
+import * as firebase from "firebase/app";
 
-AWS.config.update({
-  region: 'us-east-2',
-  endpoint: 'dynamodb.us-east-1.amazonaws.com',
-  accessKeyId: ['AKIA6QF7IXA5SRDMAQ6G'],
-  secretAccessKey: ['Claire@0217']
-});
+firebase.initializeApp({
+  apiKey: "AIzaSyC6qrSTPE2DG1GIv42CRNPivqxq7kRdVJs",
+  authDomain: "kart-8df20.firebaseapp.com",
+  projectId: "kart-8df20",
+  storageBucket: "kart-8df20.appspot.com",
+  messagingSenderId: "597913451329",
+  appId: "1:597913451329:web:3ea9f8971c6f7183f5ffc0",
+  measurementId: "G-CWPFFLWJV5"
+})
 
 export default function Home() {
   return (
