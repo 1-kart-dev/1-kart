0169f0f91b4504c5f9b7f0913a882d286ac352d0
willswinyer <will@1-kart.com>
2022-01-09 19:42:53 -0500

    dynamo configured

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,14 @@ import styles from '../styles/Home.module.scss'
 import dynamic from 'next/dynamic'
 import HomePage from './HomePage'
 import Link from 'next/link'
+import * as AWS from 'aws-sdk';
+
+AWS.config.update({
+  region: 'us-east-2',
+  endpoint: 'dynamodb.us-east-1.amazonaws.com',
+  accessKeyId: ['AKIA6QF7IXA5SRDMAQ6G'],
+  secretAccessKey: ['Claire@0217']
+});
 
 export default function Home() {
   return (
