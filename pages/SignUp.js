19a214d4b51b863381028acacb2a4bc610a2b2ae
dfmills44 <dfmills@live.unc.edu>
2022-01-09 21:46:48 -0500

    Updated with MH1-25

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import styles from '../styles/Home.module.scss'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function SignUp() {
+
+    const [username, setUser] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [conf, setConf] = useState('');
+
+    const submitUser = async() => {
+        const response = await fetch('/api/create', {
+            method: 'POST',
+            body: JSON.stringify({username, name, email, password}),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const data = await response.json();
+        console.log(data);
+    }
+
     return(
         <div className={styles.main}>
             <div className = {styles.formPage}>
@@ -13,49 +33,49 @@ export default function SignUp() {
                         <div className = {styles.form1}>
                             <label for="username">Username:</label><br></br>
                             <input className={styles.input1}
-                                id="username"
-                                name="name"
-                                type="text"
+                                type='text'
+                                value={username}
+                                onChange={e => setUser(e.target.value)}
                             /><br></br>
                         </div>
 
                         <div className = {styles.form1}>
                             <label for="name">Name:</label><br></br>
                             <input className={styles.input1}
-                                id="name"
-                                name="name"
-                                type="text"
+                                type='text'
+                                value={name}
+                                onChange={e => setName(e.target.value)}
                             /><br></br>
                         </div>
 
                         <div className = {styles.form2}>
                             <label for="email">Email Address:</label><br></br>
                             <input className={styles.input2}
-                                id="email"
-                                name="email"
-                                type="email"
+                                type='text'
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
                             /><br></br>
                         </div>
 
                         <div className = {styles.form3}>
                             <label for="password">Enter Password:</label><br></br>
                             <input className={styles.input3}
-                                id="password"
-                                name="password"
-                                type="password"
+                                type='text'
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
                             /><br></br>
                         </div>
 
                         <div className = {styles.form3}>
                             <label for="confirm">Confirm Password:</label><br></br>
                             <input className={styles.input4}
-                                id="confirm"
-                                name="confirm"
-                                type="password"
+                                type='text'
+                                value={conf}
+                                onChange={e => setConf(e.target.value)}
                             /><br></br>
                         </div>
 
-                        <button type="submit" className = {styles.formSubmit}>Submit</button>
+                        <button onClick={submitUser} className = {styles.formSubmit}>Submit</button>
                         <p className={styles.formText}>Already have an account? <a className = {styles.loginLink}><Link href = '/Login'>Login here.</Link></a></p>
                     </form>
                 </div>
