589aab2aeaa161002025e92fb76e3ae5a4335271
Andrew Mahn <andrewmahn@Andrews-MBP.lan>
2022-01-13 19:05:27 -0500

    Revamped Login and SignUp pages.

--- pages/SignUp.js
+++ pages/SignUp.js
@@ -1,53 +1,131 @@
-import React from 'react';
-import styles from '../styles/Home.module.scss'
-import Link from 'next/link'
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import styles from '../styles/Login.module.scss';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        1-Kart
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
 
 export default function SignUp() {
-    return(
-        <div className = {styles.formPage}>
-            <div className = {styles.formContainer}>
-                <form>
-                    <h2>Sign Up</h2>
-                    <div className = {styles.form1}>
-                        <label for="name">Name:</label><br></br>
-                        <input className={styles.input1}
-                            id="name"
-                            name="name"
-                            type="text"
-                        /><br></br>
-                    </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    // eslint-disable-next-line no-console
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
 
-                    <div className = {styles.form2}>
-                        <label for="email">Email Address:</label><br></br>
-                        <input className={styles.input2}
-                            id="email"
-                            name="email"
-                            type="email"
-                        /><br></br>
-                    </div>
-
-                    <div className = {styles.form3}>
-                        <label for="password">Enter Password:</label><br></br>
-                        <input className={styles.input3}
-                            id="password"
-                            name="password"
-                            type="password"
-                        /><br></br>
-                    </div>
-
-                    <div className = {styles.form3}>
-                        <label for="confirm">Confirm Password:</label><br></br>
-                        <input className={styles.input4}
-                            id="confirm"
-                            name="confirm"
-                            type="password"
-                        /><br></br>
-                    </div>
-
-                    <button type="submit">Submit</button>
-                    <p className={styles.formText}>Already have an account? <a className = {styles.loginLink}><Link href = '/Login'>Login here.</Link></a></p>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.background}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: '-6',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: '#c9e4f5' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I wanna keep up with 1Kart, send me emails!"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="Login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </div>
+  );
 }
