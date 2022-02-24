d9e0b5e559f6631b5d7db52107e25f43095b284f
andrewmahn <andrew.m.mahn@gmail.com>
2022-02-23 23:48:17 -0500

    Added temporary form to add item to kart and fixed image imports for navbar and dummy kart items

--- components/MyKart.js
+++ components/MyKart.js
@@ -1,25 +1,41 @@
-import styles from './Kart.module.scss';
+import styles from './MyKart.module.scss';
 import KartItem from './KartItem';
 import Jordans from './Jordans';
 import Beats from './Beats';
 import Fleece from './Fleece';
 import ReactNavbar from './ReactNavbar';
-import Stack from '@mui/material/stack';
+import Stack from '@mui/material/Stack';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import Grid from '@mui/material/Grid';
+import { StylesProvider } from '@material-ui/core/styles';
 
 export default function MyKart() {
     return (
-        <div className={styles.KartWrapper}>
-            <Stack container>
-                <Stack item>
-                    <Jordans />
+        <StylesProvider injectFirst>
+            <div className={styles.KartWrapper}>
+                <Stack container>
+                    <Stack item>
+                        <Jordans />
+                    </Stack>
+                    <Stack item>
+                        <Beats />
+                    </Stack>
+                    <Stack item>
+                        <Fleece />
+                    </Stack>
                 </Stack>
-                <Stack item>
-                    <Beats />
-                </Stack>
-                <Stack item>
-                    <Fleece />
-                </Stack>
-            </Stack>
-        </div>
+                <Grid container className={styles.footer}>
+                    <Grid item xs={1} sm={2} md={3} lg={4} />
+                    <Grid item xs={5} sm={4} md={3} lg={2}>
+                        <button className={styles.button}><Link href='/NewItem'>Add to Kart</Link></button>
+                    </Grid>
+                    <Grid item xs={5} sm={4} md={3} lg={2}>
+                        <button className={styles.button}><Link href='.'>Checkout</Link></button>
+                    </Grid>
+                    <Grid item xs={1} sm={2} md={3} lg={4} />
+                </Grid>
+            </div>
+        </StylesProvider>
     );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,10 @@
 import styles from './Navbar.module.scss';
-import Image from 'next/Image';
-import Link from 'next/Link';
+import Image from 'next/image';
+import Link from 'next/link';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import Grid from '@mui/material/Grid';
 import LogoutIcon from '@mui/icons-material/Logout';
+import logo from '../public/1kartCircle.jpg';
 
 export default function Navbar() {
     // const [width, setWidth] = useState(null);
@@ -18,7 +19,7 @@ export default function Navbar() {
     return (
         <Grid container className={styles.nav}>
             <Grid item xs={1} align="center">
-                <Link href="/"><Image src="/../public/1kartCircle.jpg" width="70px" height="70px" alt="logo" className={styles.image}/></Link>
+                <Link href="/"><Image src={logo} width="70px" height="70px" alt="logo" className={styles.image}/></Link>
             </Grid>
             <Grid item xs={7} sm={8} md={9}>
                 <ul className={styles.nav_links} sx={{ overflow: 'auto' }}>
