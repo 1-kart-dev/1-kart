5f71b2b29e97929e6b5fd6733bbcc693ed891332
Robert Sullivan <rsullivan@unc.edu>
2022-03-09 17:50:13 -0500

    Getting started, initial conversation

---
+++ pages/Account/ShoppingLists.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './NewAccount.module.scss';
+
+export default function ShoppningLists() {
+    return(
+        <div className={styles.container}>
+            <h2 className={styles.rob1}>Shopping List</h2>
+        </div>
+    );
+}
