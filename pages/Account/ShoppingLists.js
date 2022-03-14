05468cc00d83da3f067a0db96d4f48329ef63c51
Robert Sullivan <rsullivan@unc.edu>
2022-03-13 23:50:18 -0500

    continued work

--- pages/Account/ShoppingLists.js
+++ pages/Account/ShoppingLists.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react' 
 import styles from './NewAccount.module.scss';
 
 export default function ShoppningLists() {
     return(
         <div className={styles.container}>
-            <h2 className={styles.rob1}>Shopping List</h2>
+            <h2 className={styles.header}>Restyled Shopping List</h2>
+            <h3 className={styles.subheader}>Items of Interest</h3>
+            <h4 className={styles.button}>Order Inventory</h4>
         </div>
     );
 }
