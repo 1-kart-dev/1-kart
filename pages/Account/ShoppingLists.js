125fb2e6c4c97268faadb28f0219e121d418ca94
Robert Sullivan <rsullivan@unc.edu>
2022-03-16 14:33:15 -0500

    Button now functional, added a link, layout fixes

--- pages/Account/ShoppingLists.js
+++ pages/Account/ShoppingLists.js
@@ -9,14 +9,21 @@ export default function ShoppningLists()
 {
     return(
         <div className={styles.container}>
-            <h2 className={styles.header}>Restyled Shopping List</h2>
-            <h3 className={styles.subheader}>Items of Interest</h3>
-            <h4 className={styles.button}>Currently, we are working hard to improve the layout of our website. Our team will have the final product very shortly. Thank you for your consideration. Go 1Kart.</h4>
+            <h1 className={styles.header}>Restyled Shopping List</h1>
+            <h2 className={styles.subheader}>Items of Interest</h2>
+            <h3 className={styles.button}>Currently, we are working hard to improve the layout of our website. Our team will have the final product very shortly. Thank you for your consideration. Go 1Kart.</h3>
+            <h4 className={styles.subheader}>Here at 1Kart, we are a team of</h4>
+            <ul>
+            <li className={styles.list}>technologists</li>
+            <li className={styles.list}>thinkers</li>
+            <li className={styles.list}>builders</li>
+            </ul>
             <Stack>
-                <IconButton size="large" color="secondary" aria-label="add to shopping cart">
+                <IconButton href="http://localhost:3000/NewItem" color="primary" aria-label="add to shopping cart">
                     <AddShoppingCartIcon fontSize="large"/>
                 </IconButton>
              </Stack>
+             <a href="http://localhost:3000/Account" className={styles.header}>main</a>
         </div>
     );
 }
