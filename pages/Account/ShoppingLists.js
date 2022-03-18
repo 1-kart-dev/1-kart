79e984b56f22dbd0c66fe8ab8bbe3b44ffa208b0
Robert Sullivan <rsullivan@unc.edu>
2022-03-18 11:44:44 -0500

    I made ushno smaller lol

--- pages/Account/ShoppingLists.js
+++ pages/Account/ShoppingLists.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Image from 'next/Image'
 
 export default function ShoppningLists() 
 {
@@ -26,7 +27,12 @@ export default function ShoppningLists()
              </Stack>
              <h3 className={styles.subheader2}>Otherwise, feel free to navigate back to the <a href="http://localhost:3000/Account">main</a> page </h3>
             <center>
-                <img src="/pfp.jpg" alt="Ushno" />
+                <Image 
+                    src="/pfp.jpg"
+                    alt="Ush"
+                    width={100}
+                    height={100}
+                />
             </center>
         </div>
     );
