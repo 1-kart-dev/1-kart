5b00923ab4f743aeaecd0e4a8abf3cae7e8013eb
Robert Sullivan <rsullivan@unc.edu>
2022-04-04 15:25:44 -0400

    columns

--- pages/Account/ShoppingLists.js
+++ pages/Account/ShoppingLists.js
@@ -11,14 +11,13 @@ export default function ShoppningLists()
     return(
         <div className={styles.container}>
             <h1 className={styles.header}>Your Lists</h1>
-            <h2 className={styles.subheader}>Items of Interest</h2>
             <h3 className={styles.subheader2}>Want to add something to your cart? Try our add to shopping cart feature!</h3>
             <Stack>
                 <IconButton href="http://localhost:3000/NewItem" color="primary" aria-label="add to shopping cart">
                     <AddShoppingCartIcon fontSize="large"/>
                 </IconButton>
              </Stack>
-             <h3 className={styles.subheader2}>Otherwise, feel free to navigate back to the <a href="http://localhost:3000/Account">main</a> page </h3>
+            <h3 className={styles.subheader2}>Otherwise, feel free to navigate back to the <a href="http://localhost:3000/Account">main</a> page </h3>
             <center>
                 <Image 
                     src="/pfp.jpg"
@@ -26,13 +25,13 @@ export default function ShoppningLists()
                     width={100}
                     height={100}
                 />
-            </center>
+            </center> */
             <div className={styles.content}>
                 <div className={styles.shoppingLists}>
-                    <h2>Shopping Lists</h2>
+                    <h2 className={styles.shoppingLists}>Shopping Lists</h2>
                 </div>
                 <div className={styles.wishLists}>
-                    <h2 className={styles.column}>Wish Lists</h2>
+                    <h2 className={styles.wishLists}>Wish Lists</h2>
                 </div>
             </div>
         </div>     
